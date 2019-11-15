64e02cce6f1d398a8cd34549b635dd2104882bf4
Nagellan <31626879+Nagellan@users.noreply.github.com>
2019-11-15 21:53:16 +0300

    Fix bug with panel info rendering * Now everything looks fine and work it should do;

--- src/components/StatsPanel/index.js
+++ src/components/StatsPanel/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { observable, decorate, action } from 'mobx';
+import { observable, decorate } from 'mobx';
 import { observer } from 'mobx-react'
 // import project storage
 import pokeStore from '../../stores/pokeStore'
@@ -19,34 +19,38 @@ const StatsPanel = observer(
     }
 
     updateAbilities(name) {
-      pokeStore.getPokemonInfo(name).then(async response => 
-        await response.abilities.map((abil) => 
+      pokeStore.getPokemonInfo(name).then(async response => {
+        let abils = await response.abilities.map((abil) =>
           pokeStore.getAbilityInfo(abil.ability.url.split('/').reverse()[1])
-            .then(async (res) => {
-              this.abilities = [<p key={abil.ability.name} className="skill">{abil.ability.name}</p>, <p key={abil.ability.name + "descr"} className="descr">{res.effect_entries[0].effect}</p>]
-            })
-        )
-      );
+            .then(async (res) => await [
+              <p key={abil.ability.name+"skill"} className="skill">{abil.ability.name}</p>,
+              <p key={abil.ability.name+"descr"} className="descr">{res.effect_entries[0].effect}</p>
+            ])
+        );
+        Promise.all(abils).then((abilsRes) => {
+          this.abilities = (
+            <div key="abilities" className="stats-block">
+              <p key="abilities-title" className="title">Abilities</p>
+              {abilsRes}
+            </div>
+          );
+        });
+      });
     }
+
+
 
     updatePokemonInfo(name) {
       pokeStore.getPokemonInfo(name).then(response => {
         this.pokemonInfo = (
-          <div className="info-wrapper">
-            <div key="statistics" className="stats-block">
-              <p className="title">Statistics</p>
-              {response.stats.slice().reverse().map((stat) =>
-                <p key={stat.stat.name}>
-                  <span className="char">{stat.stat.name}</span>:
-                  <span className="qual"> {stat.base_stat}</span>
-                </p>
-              )}
-            </div>
-
-            <div key="abilities" className="stats-block">
-              <p className="title">Abilities</p>
-              {this.abilities}
-            </div>
+          <div key="statistics" className="stats-block">
+            <p key="statistics-title" className="title">Statistics</p>
+            {response.stats.slice().reverse().map((stat) =>
+              <p key={stat.stat.name}>
+                <span key={stat.stat.name+"char"} className="char">{stat.stat.name}</span>:
+                <span key={stat.stat.name+"qual"} className="qual"> {stat.base_stat}</span>
+              </p>
+            )}
           </div>
         );
       });
@@ -56,7 +60,10 @@ const StatsPanel = observer(
       return (
         <aside className="StatsPanel">
           <div className="pokemon-name">{pokeStore.currentPokemonName}</div>
-          {this.pokemonInfo}
+          <div className="info-wrapper">
+            {this.pokemonInfo}
+            {this.abilities}
+          </div>
         </aside>
       );
     }

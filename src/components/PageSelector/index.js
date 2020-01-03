e398de3dea78481d0cb7932a7a0399a27e436f42
Nagellan <31626879+Nagellan@users.noreply.github.com>
2020-01-03 22:34:11 +0300

    Add components structure & styles

--- src/components/PageSelector/index.js
+++ src/components/PageSelector/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { computed, decorate } from 'mobx';
 import { observer } from 'mobx-react'
 // import project storage
 import pokeStore from '../../stores/pokeStore'
@@ -6,24 +7,49 @@ import pokeStore from '../../stores/pokeStore'
 import './style.css';
 
 class PageSelector extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("props");
+  get lastPage() {
+    return Math.ceil(pokeStore.numOfPokemons/pokeStore.cardsPerPage);
   }
+
   render() {
+    function PageButton(props) {
+      return (
+        <div
+          className={"page " + props.addClass + (+props.num === pokeStore.currentPage ? " active" : "")}
+        >
+          { props.num }
+        </div>
+      );
+    }
+
     return (
       <div id="PageSelector">
-        {
-          pokeStore.currentPage === 1 || pokeStore.currentPage === 2
-          ? <div>1 | 2 | 3 | 4 | {Math.ceil(pokeStore.numOfPokemons/pokeStore.cardsPerPage)}</div>
-          : pokeStore.currentPage === Math.ceil(pokeStore.numOfPokemons/pokeStore.cardsPerPage) || pokeStore.currentPage === Math.ceil(pokeStore.numOfPokemons/pokeStore.cardsPerPage) - 1
-            ? <div>1 | {Math.ceil(pokeStore.numOfPokemons/pokeStore.cardsPerPage) - 3} | {Math.ceil(pokeStore.numOfPokemons/pokeStore.cardsPerPage) - 2} | {Math.ceil(pokeStore.numOfPokemons/pokeStore.cardsPerPage) - 1} | {Math.ceil(pokeStore.numOfPokemons/pokeStore.cardsPerPage)}</div>
-            : <div>1 | {pokeStore.currentPage - 1} | {pokeStore.currentPage} | {pokeStore.currentPage + 1} | {Math.ceil(pokeStore.numOfPokemons/pokeStore.cardsPerPage)}</div>
-        }
+        <PageButton num="1" addClass="first" />
+
+        <div className="pages-closest">
+          {
+            pokeStore.currentPage === 1 || pokeStore.currentPage === 2
+            ? [ <PageButton num="2" key="page-num-2" />, "|" ,
+                <PageButton num="3" key="page-num-3" />, "|" ,
+                <PageButton num="4" key="page-num-4" /> ]
+            : pokeStore.currentPage === this.lastPage || pokeStore.currentPage === this.lastPage - 1
+              ? [ <PageButton num={ this.lastPage - 3 } key={"page-num-" + (this.lastPage - 3) } />, "|",
+                  <PageButton num={ this.lastPage - 2 } key={"page-num-" + (this.lastPage - 2) } />, "|",
+                  <PageButton num={ this.lastPage - 1 } key={"page-num-" + (this.lastPage - 1) } /> ]
+              : [ <PageButton num={ pokeStore.currentPage - 1 } key={"page-num-" + (pokeStore.currentPage - 1) } />, "|",
+                  <PageButton num={ pokeStore.currentPage     } key={"page-num-" + (pokeStore.currentPage)     } />, "|",
+                  <PageButton num={ pokeStore.currentPage + 1 } key={"page-num-" + (pokeStore.currentPage + 1) } /> ]
+          }
+        </div>
+
+        <PageButton num={ this.lastPage } addClass="last" />
       </div>
     );
   }
- 
 }
+
+decorate(PageSelector, {
+  lastPage: computed,
+});
 
 export default observer(PageSelector);
